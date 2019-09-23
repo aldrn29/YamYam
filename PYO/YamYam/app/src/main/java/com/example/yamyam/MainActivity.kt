94fd41f90d0ec879857dc81c214e848f67ff869c
pjj9606 <55330623+pjj9606@users.noreply.github.com>
2019-09-24 05:28:00 +0900

    PYO

--- PYO/YamYam/app/src/main/java/com/example/yamyam/MainActivity.kt
+++ PYO/YamYam/app/src/main/java/com/example/yamyam/MainActivity.kt
@@ -1,30 +1,48 @@
 package com.example.yamyam
 
+import android.content.Intent
+import android.os.Build.VERSION_CODES.O
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val textView = TextView(this)
         //플러스버튼 클릭리스너
         plusButton.setOnClickListener{
-            textView.text = "플러스 버튼 눌림"
+            val intent = Intent(this, input_material_activity::class.java)
+            startActivityForResult(intent, 1)
         }
+
         //마이너스버튼 클릭리스너
         minusButton.setOnClickListener{
-            textView.text = "마이너스 버튼 눌림"
+            //textView.text = "마이너스 버튼 눌림"
         }
-        upperBody.addView(textView, 0)
+
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val textView = TextView(this)
+        upperBody.addView(textView, 0)
+
+        if (resultCode == RESULT_OK){
+            //인텐트에서 nameOfMaterial 받아와서
+            val nameOfMaterial = data?.getStringExtra("nameOfMaterial")
 
 
+           // Toast.makeText(this,"$nameOfMaterial", Toast.LENGTH_SHORT).show()
+            textView.text = "$nameOfMaterial"
+        }
+
+    }
 }
---
+++ PYO/YamYam/app/src/main/java/com/example/yamyam/input_material_activity.kt
@@ -0,0 +1,25 @@
+package com.example.yamyam
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_input_material_activity.*
+
+class input_material_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_material_activity)
+
+
+        val resultIntent = Intent(this,MainActivity::class.java)
+
+        addButton.setOnClickListener{
+            //"nameOfMaterial" to material_text.text.toString()
+            resultIntent.putExtra("nameOfMaterial", material_text.text.toString())
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
+    }
+}
