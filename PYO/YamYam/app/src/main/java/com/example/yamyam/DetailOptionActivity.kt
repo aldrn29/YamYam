abc61e21326eec0d310ef3ef47f0fe28436585b1
aldrn <aldrn29@gmail.com>
2019-10-01 12:05:50 +0900

    세부옵션 class 생성, 적용되지 않은 class 이름 수정

---
+++ PYO/YamYam/app/src/main/java/com/example/yamyam/DetailOptionActivity.kt
@@ -0,0 +1,14 @@
+package com.example.yamyam
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_detail_option.*
+
+class DetailOptionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+    }
+}
