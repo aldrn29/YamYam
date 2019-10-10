ae36a74eaff40555b5acdd532936d92a72f74884
aldrn <aldrn29@gmail.com>
2019-10-10 21:53:18 +0900

    Fragment 추가, Tab 클릭 시 Fragment 화면 전환 구현

--- PYO/YamYam/app/src/main/java/com/example/yamyam/MainActivity.kt
+++ PYO/YamYam/app/src/main/java/com/example/yamyam/MainActivity.kt
@@ -3,9 +3,19 @@ package com.example.yamyam
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import androidx.fragment.app.FragmentTransaction
+import android.view.MenuItem
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.annotation.NonNull
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         /* + - 버튼 클릭 리스너 */
         upperPlusButton.setOnClickListener{
@@ -31,13 +42,43 @@ class MainActivity : AppCompatActivity() {
             //textView.text = "마이너스 버튼 눌림"
         }
 
-//        임시 버튼 하단 탭 구성전에 임시로 사용중 버튼 위치상 첫번째 냉동고 기입 식재료명 가려짐
+        //임시 버튼 하단 탭 구성전에 임시로 사용중 버튼 위치상 첫번째 냉동고 기입 식재료명 가려짐
         temporaryButton.setOnClickListener {
             val intent = Intent(this, RecipeSerachActivity::class.java)
             startActivity(intent)
         }
-    }
 
+        // 하단 탭부분 클릭 시 Fragment 전환
+        val bottomNavigationView : BottomNavigationView = findViewById(R.id.navigationView)
+        val transaction : FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.act_fragment, FridgeFragment()).commit()
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.fridgeItem -> {
+                    Toast.makeText(this, "첫번째", Toast.LENGTH_SHORT).show()
+                    val transaction : FragmentTransaction = supportFragmentManager.beginTransaction()
+                    transaction.add(R.id.act_fragment, FridgeFragment())
+                    transaction.commit()
+                }
+                R.id.heartItem -> {
+                    Toast.makeText(this, "두번째", Toast.LENGTH_SHORT).show()
+                    val transaction : FragmentTransaction = supportFragmentManager.beginTransaction()
+                    transaction.add(R.id.act_fragment, HeartFragment())
+                    transaction.commit()
+                }
+                R.id.searchItem -> {
+                    Toast.makeText(this, "세번째", Toast.LENGTH_SHORT).show()
+                    val transaction : FragmentTransaction = supportFragmentManager.beginTransaction()
+                    transaction.add(R.id.act_fragment, SearchFragment())
+                    transaction.commit()
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+
+    }
     var upperAdapter: MaterialAdapter? = null
     var lowerAdapter: MaterialAdapter? = null
     var upperMaterialsList = ArrayList<Material>()
