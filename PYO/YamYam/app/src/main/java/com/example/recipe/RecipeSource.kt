46bf1e69830e4afe1493d583620da3e301737453
Yee-Kun Jung <48750313+Twoketchupplz@users.noreply.github.com>
2019-11-16 03:31:54 +0900

    FB에 데이터 입력, 사진크기 자동조정

--- PYO/YamYam/app/src/main/java/com/example/recipe/RecipeSource.kt
+++ PYO/YamYam/app/src/main/java/com/example/recipe/RecipeSource.kt
@@ -1,10 +1,20 @@
 package com.example.recipe
 
+import com.google.firebase.database.Exclude
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
 data class RecipeSource(
-    var title: String= "",
     var img : String = "",
-    var name : String = ""
-)
+    var name : String = "",
+    var description: String = ""
+) {
+    @Exclude
+    fun toMap() : Map<String, Any?> {
+        return mapOf(
+            "img" to img,
+            "name" to name,
+            "description" to description
+        )
+    }
+}
